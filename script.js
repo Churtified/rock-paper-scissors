7d681c6db06ab48e7bd63f387f1752c960c389e9
Churtified <88999504+Churtified@users.noreply.github.com>
2022-06-28 20:15:13 +0930

    finish code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        resultsDisplay.textContent = 'It\'s a tie!';
+        resultsDisplay.textContent = 'Round Result = It\'s a tie!';
     }
     else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
@@ -20,7 +20,7 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
         playerScore++;
-        resultsDisplay.textContent = 'You Win!';
+        resultsDisplay.textContent = 'Round Result = You Win!';
         playerTally.textContent = `${playerScore}`;
     }
     else if (
@@ -29,27 +29,10 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection === 'scissors')
     ) {
         computerScore++;
-        resultsDisplay.textContent = 'You Lose!';
+        resultsDisplay.textContent = 'Round Result = You Lose!';
         computerTally.textContent = `${computerScore}`;
     }
 }
-
-/*function game() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection = prompt('Make your choice!').toLowerCase();
-        let computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-    }
-    if (playerScore > computerScore) {
-        console.log('You win the game!');
-    }
-    else if (playerScore < computerScore) {
-        console.log('You lose the game!');
-    }
-    else if (playerScore === computerScore) {
-        console.log('Tie game!');
-    }
-} */
 
 //ui
 
@@ -59,12 +42,32 @@ const scissorsBtn = document.querySelector('#scissorsBtn')
 const resultsDisplay = document.querySelector('#resultsDisplay')
 const playerTally = document.querySelector('#playerTally')
 const computerTally = document.querySelector('#computerTally')
+const announceWinner = document.querySelector('#announceWinner')
 
-rockBtn.addEventListener('click', () => startGame('rock'))
-paperBtn.addEventListener('click', () => startGame('paper'))
-scissorsBtn.addEventListener('click', () => startGame('scissors'))
+rockBtn.addEventListener('click', () => game('rock'))
+paperBtn.addEventListener('click', () => game('paper'))
+scissorsBtn.addEventListener('click', () => game('scissors'))
 
-function startGame(playerSelection) {
+function game(playerSelection) {
+    announceWinner.textContent = '';
     let computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    if (finishGame()) {
+        if (playerScore === 5) {
+            announceWinner.textContent = 'You Win the first to 5! Make another selection to play another game.';
+        }
+        else if (computerScore === 5) {
+            announceWinner.textContent = 'You Lose the first to 5! Make another selection to play another game.';
+        }
+        playerScore = 0;
+        computerScore = 0;
+        playerTally.textContent = `${playerScore}`;
+        computerTally.textContent = `${computerScore}`;
+    }
+}
+
+function finishGame() {
+    if (playerScore === 5 || computerScore === 5) {
+        return true;
+    }
 }
