30df0a7ee60e32b2c46c5300bad6fd8712c8c1f6
Churtified <88999504+Churtified@users.noreply.github.com>
2022-06-28 12:00:02 +0930

    finish refactor

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function computerSelection {
+function computerPlay() {
     let selection = Math.floor(Math.random() * 3);
     if (selection === 0) return 'rock';
     if (selection === 1) return 'paper';
     if (selection === 2) return 'scissors';
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log('Tie!');
+    }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        console.log('You Win!');
+    }
+    else if (
+        (playerSelection === 'scissors' && computerSelection === 'rock') ||
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors')
+    ) {
+        computerScore++;
+        console.log('You Lose!');
+    }
+}
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        let playerSelection = prompt('Make your choice!').toLowerCase();
+        let computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        console.log('You win the game!');
+    }
+    else if (playerScore < computerScore) {
+        console.log('You lose the game!');
+    }
+    else if (playerScore === computerScore) {
+        console.log('Tie game!');
+    }
 }
