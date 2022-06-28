b14bd641c2f9e18230ab9d5e93a1d586c0a09888
Churtified <88999504+Churtified@users.noreply.github.com>
2022-06-28 12:42:09 +0930

    add score tally

--- script.js
+++ script.js
@@ -1,64 +1,70 @@
+//game
+
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection;
-let computerSelection;
 
 function computerPlay() {
     let selection = Math.floor(Math.random() * 3);
-    if (selection === 0) {
-        return ('rock');
-    }
-    else if (selection === 1) {
-        return ('paper');
-    }
-    else if (selection === 2) {
-        return ('scissors');
-    }
+    if (selection === 0) return 'rock';
+    if (selection === 1) return 'paper';
+    if (selection === 2) return 'scissors';
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    if (playerSelection === computerSelection) {
+        resultsDisplay.textContent = 'It\'s a tie!';
+    }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
         playerScore++;
-        console.log('You Win! Rock beats Scissors');
+        resultsDisplay.textContent = 'You Win!';
+        playerTally.textContent = `${playerScore}`;
     }
-    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    else if (
+        (playerSelection === 'scissors' && computerSelection === 'rock') ||
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors')
+    ) {
         computerScore++;
-        console.log('You Lose! Paper beats Rock');
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++;
-        console.log('You Win! Paper beats Rock');
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        console.log('You Lose! Paper beats Scissors');
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++;
-        console.log('You Win! Scissors beats Paper');
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        computerScore++;
-        console.log('You Lose! Rock beats Scissors');
-    }
-    else {
-        console.log('Tie!');
+        resultsDisplay.textContent = 'You Lose!';
+        computerTally.textContent = `${computerScore}`;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter your selection").toLowerCase();
+/*function game() {
+    for (i = 0; i < 5; i++) {
+        let playerSelection = prompt('Make your choice!').toLowerCase();
         let computerSelection = computerPlay();
         playRound(playerSelection, computerSelection);
     }
     if (playerScore > computerScore) {
-        console.log('You win the best of 5!');
+        console.log('You win the game!');
     }
     else if (playerScore < computerScore) {
-        console.log('You lose the best of 5!');
+        console.log('You lose the game!');
     }
-    else {
-        console.log('It\'s a draw!');
+    else if (playerScore === computerScore) {
+        console.log('Tie game!');
     }
+} */
+
+//ui
+
+const rockBtn = document.querySelector('#rockBtn')
+const paperBtn = document.querySelector('#paperBtn')
+const scissorsBtn = document.querySelector('#scissorsBtn')
+const resultsDisplay = document.querySelector('#resultsDisplay')
+const playerTally = document.querySelector('#playerTally')
+const computerTally = document.querySelector('#computerTally')
+
+rockBtn.addEventListener('click', () => startGame('rock'))
+paperBtn.addEventListener('click', () => startGame('paper'))
+scissorsBtn.addEventListener('click', () => startGame('scissors'))
+
+function startGame(playerSelection) {
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
 }
